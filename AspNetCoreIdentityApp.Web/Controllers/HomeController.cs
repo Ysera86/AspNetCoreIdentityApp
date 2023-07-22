b412eaec5dd93231d2b30df3d6272cb996dbf6c0
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-22 19:48:04 +0300

    Add : Implement signup view and endpoint

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -20,6 +24,40 @@ namespace AspNetCoreIdentityApp.Web.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            //normalde burdaki kodları servis katmanında yazardık, ama proje n katmanlı değil.
+
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            // hash : Password12 --> 32423l4jşlmfcldşsmfşlksjfmşsdof 
+
+            var identityResult = await _userManager.CreateAsync(new() { UserName = request.UserName, PhoneNumber = request.Phone, Email = request.Email }, request.Password); // passwordu hashlicak (geri döndürülemez):güvenli
+
+            if (identityResult.Succeeded)
+            {
+                //ViewBag.SuccessMessage = "Üyelik kayıt işlemi başarılı"; // aşağıda get actipnmethoduna yolluoruz elementlerin içleri boşalsın diye ama o zaman 2.requedt olcağı için ViewBag içeriği kaybolacak. O nedenle Tempdataya geçtik.
+
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarılı"; // Framework tarafından cookiede tek seferlik olarak taşınıyor.
+
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach (IdentityError error in identityResult.Errors)
+            {
+                //ModelState.AddModelError("UserName",error.Description); // dersem username tb altında görünür ama bunun en üstte görünmesini istiyorum
+                ModelState.AddModelError(string.Empty, error.Description); // ing şuan
+            }
+
             return View();
         }
 
---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+            
+        }
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [DisplayName("Kullanıcı Adı :")]
+        public string UserName { get; set; }
+
+        [DisplayName("Email :")]
+        public string Email { get; set; }
+
+        [DisplayName("Telefon :")]
+        public string Phone { get; set; }
+
+        [DisplayName("Şifre :")]
+        public string Password { get; set; }
+
+        [DisplayName("Şifre Tekrar :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
